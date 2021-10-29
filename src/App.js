2894f0c5885e84b9c857ae49187039f4d720c120
sontus <sontuschandraanik.cse@gmail.com>
2021-10-29 16:58:16 +0600

    Template Mastering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <Switch>
+            <Route exact path="/">
+                <Home></Home>
+            </Route>
+            {/* About us */}
+            <Route path="/about">
+              <About></About>
+            </Route>
+            {/* Contact us */}
+            <Route path="/contact">
+              <Contact></Contact>
+            </Route>
+        </Switch>
+        <Footer></Footer>
+      </Router>
     </div>
   );
 }
